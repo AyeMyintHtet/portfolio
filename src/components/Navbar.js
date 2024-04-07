187e3c841b1385d498a2ba51a710ea24dcc78460
unknown <ayemyinthtet099@gmail.com>
2024-04-08 04:57:12 +0630

    update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -94,7 +94,7 @@ function NavBar() {
 
             <Nav.Item>
               <Nav.Link
-                href="https://.vercel.app/"
+                href="https://bits-of-code-kappa.vercel.app/"
                 target="_blank"
                 rel="noreferrer"
               >
